ae4e784a480bcdbad9450e55768b3f31aaabe564
WaleedYaser <waleedyaser95@gmail.com>
2020-01-31 11:34:01 +0200

    feat: implement mat4_inverse

--- math/include/math/Mat4.h
+++ math/include/math/Mat4.h
@@ -45,6 +45,21 @@ namespace math
 	}
 
 	inline static Mat4
+	mat4_new(
+		float m00, float m01, float m02, float m03,
+		float m10, float m11, float m12, float m13,
+		float m20, float m21, float m22, float m23,
+		float m30, float m31, float m32, float m33)
+	{
+		Mat4 A;
+		A[0][0] = m00, A[0][1] = m01, A[0][2] = m02, A[0][3] = m03;
+		A[1][0] = m10, A[1][1] = m11, A[1][2] = m12, A[1][3] = m13;
+		A[2][0] = m20, A[2][1] = m21, A[2][2] = m22, A[2][3] = m23;
+		A[3][0] = m30, A[3][1] = m31, A[3][2] = m32, A[3][3] = m33;
+		return A;
+	}
+
+	inline static Mat4
 	mat4_identity()
 	{
 		Mat4 A;
@@ -68,7 +83,68 @@ namespace math
 	inline static Mat4
 	mat4_inverse(const Mat4 &A)
 	{
-		assert(false && "Not implemented");
-		return Mat4{}; 
+		// clone A
+		Mat4 M = A;
+		// result
+		Mat4 R = mat4_identity();
+		for (uint8_t column = 0; column < 4; ++column)
+		{
+			// swap row in case out pivot point is not working
+			if (M[column][column] == 0.0f)
+			{
+				uint8_t row_max = column;
+				for (uint8_t row = 0; row < 4; ++row)
+					if (fabsf(M[row][column]) > fabsf(M[row_max][column]))
+						row_max = row;
+				if (row_max = column)
+				{
+					// TODO: singular matrix
+					assert(false && "singular matrix");
+				}
+				// Swap rows
+				else
+				{
+					for (uint8_t j = 0; j < 4; ++j)
+					{
+						float tmp = M[column][j];
+						M[column][j] = M[row_max][j];
+						M[row_max][j] = tmp;
+
+						tmp = R[column][j];
+						R[column][j] = R[row_max][j];
+						R[row_max][j] = tmp;
+					}
+				}
+			}
+
+			// Set each row in the column to 0
+			for (uint8_t row = 0; row < 4; ++row)
+			{
+				if (row != column)
+				{
+					float coeff = M[row][column] / M[column][column];
+					if (coeff != 0.0f)
+					{
+						for (uint8_t j = 0; j < 4; ++j)
+						{
+							M[row][j] -= coeff * M[column][j];
+							R[row][j] -= coeff * R[column][j];
+						}
+						// Set the element to 0 for safety
+						M[row][column] = 0.0f;
+					}
+				}
+			}
+		}
+
+		// Set each element of the diagonal to 1
+		for (uint8_t row = 0; row < 4; ++row)
+		{
+			for (uint8_t column = 0; column < 4; ++column)
+			{
+				R[row][column] /= M[row][row];
+			}
+		}
+		return R;
 	}
 }
--- playground/src/playground.cpp
+++ playground/src/playground.cpp
@@ -8,10 +8,21 @@ using namespace math;
 int
 main(int argc, char *argv[])
 {
-	Mat4 A = mat4_identity();
-	Mat4 B = A;
+	Vec3 a = Vec3{0.0f, 1.0f, 2.0f};
 
-	B[0][0] = 10;
+	Mat4 A = mat4_identity();
+	Mat4 B = mat4_identity();
+	Mat4 C = A * B;
+
+	Mat4 D = mat4_new(
+		0.707107, 0, -0.707107, 0,
+		-0.331295, 0.883452, -0.331295, 0,
+		0.624695, 0.468521, 0.624695, 0,
+		4.000574, 3.00043, 4.000574, 1); 
+
+	Mat4 D_inverted = mat4_inverse(D);
+
+	Mat4 I = D * D_inverted;
 
 	return 0;
 }
